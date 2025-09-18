2ffba16955501d9047320bab34a946237951b9d7
cheng960313-svg <cheng960313@gmail.com>
2025-09-18 15:46:50 +0800

    main5.c

---
+++ EX2/main5.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main() {
+ char grade ="b'
+ printf ("Your grade is %c\n" , grade);
+switch(grade){
+case 'A':
+  printf("excellent!\n");
+  break;
+case 'B';
+case 'C';
+  [rintf("well done\n");
+  break;
+case'D':
+case'F';
+  printf("better try again!\n");
+  break;
+default:
+  printf("invali grade\n");
+}
+return 0;
+}
