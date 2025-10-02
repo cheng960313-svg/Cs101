179bb57457dfe010b18194297dc9a2cac1efb7b9
cheng960313-svg <cheng960313@gmail.com>
2025-10-02 18:17:21 +0800

    Create main3.c

---
+++ EX3/main3.c
@@ -0,0 +1,16 @@
+int main(){
+    int i=2;
+    if (i %15 ==0){
+        printf("LOVE IU");
+    }
+    else if (i%3==0){
+        printf("LOVE");
+    }
+    else if (i%5==0){
+        printf("IU");
+    }
+    else{
+        printf("%d\n",i);
+    }
+    return 0;
+}
